dc589ace680c20a39af65f6fc4fbd04e9e94ae26
Frarif <freakinarif25@gmail.com>
2023-02-01 14:11:11 +0700

    add: perimeter

--- triangle/index.js
+++ triangle/index.js
@@ -2,5 +2,7 @@ function calculateTriangleArea() {
   const base = parseInt(document.getElementById("base").value);
   const height = parseInt(document.getElementById("height").value);
   const area = (base * height) / 2;
+  const perimeter = base * 3;
   document.getElementById("area").value = area + " m²";
+  document.getElementById("perimeter").value = perimeter + " m";
 }
