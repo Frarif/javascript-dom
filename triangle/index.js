6f0dff67847737fc255ed949ec4d3de60d848ef5
M Haidar Hanif <git@mhaidarhanif.com>
2023-02-01 21:41:38 +0700

    refactor: user form

--- triangle/index.js
+++ triangle/index.js
@@ -1,8 +1,17 @@
+const triangleForm = document.getElementById("triangle-form");
+
+triangleForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  calculateTriangleAreaAndPerimeter();
+});
+
 function calculateTriangleAreaAndPerimeter() {
   const base = parseInt(document.getElementById("base").value);
   const height = parseInt(document.getElementById("height").value);
   const area = (base * height) / 2;
   const perimeter = base * 3;
+
   document.getElementById("area").value = area + " m²";
   document.getElementById("perimeter").value = perimeter + " m";
 }
