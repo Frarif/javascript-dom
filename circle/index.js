733814392bc469add636ca671b731e322755f99b
Frarif <freakinarif25@gmail.com>
2023-02-01 14:25:29 +0700

    create: perimeter function on circle

--- circle/index.js
+++ circle/index.js
@@ -1,5 +1,7 @@
 function calculateCircleArea() {
   const radius = parseInt(document.getElementById("radius").value);
   const area = Math.PI.toFixed(2) * radius ** 2;
+  const perimeter = 2 * Math.PI.toFixed(2) * radius;
   document.getElementById("area").value = ` ${area.toFixed(2)} m²`;
+  document.getElementById("perimeter").value = ` ${perimeter.toFixed(2)} m`;
 }
